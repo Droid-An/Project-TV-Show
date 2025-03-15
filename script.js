b72a20f39db7c263ddaa422ff7548adae711dfa1
Droid-An <andrei.filippov.aaf@gmail.com>
2025-03-15 14:16:21 +0000

    rendered first card

--- script.js
+++ script.js
@@ -2,7 +2,28 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  const firstEpisode = allEpisodes[0];
+
+  const template = document.getElementById("film-card");
+  const createFilmCard = (firstEpisode) => {
+    const card = template.content.cloneNode(true);
+
+    // Now we are querying our cloned fragment, not the entire page.
+    const { name, season, number, image, summary } = firstEpisode;
+    const title = `${name} - S${String(season).padStart(2, "0")}E${String(number).padStart(2, "0")}`
+    card.querySelector("h3").textContent = title;
+    card.querySelector("summary").textContent = summary.replace(/<\/?p>/g, "");
+    // console.log(image.medium)
+    card.querySelector("img").setAttribute("src", image.medium);
+    // Return the card, rather than directly appending it to the page
+    return card;
+  };
+  const filmCard = createFilmCard(firstEpisode);
+
+  // Remember we need to append the card to the DOM for it to appear.
+  document.body.append(filmCard);
 }
+
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
