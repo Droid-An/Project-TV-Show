7ffcc2ed02cfdf5e73e951e92c7303ea7bb04cc6
Droid-An <andrei.filippov.aaf@gmail.com>
2025-03-15 15:25:51 +0000

    showed all episodes and added footer

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-//You can edit ALL of the code here
+//Create film card
+const template = document.getElementById("film-card");
+const createFilmCard = (film) => {
+  const card = template.content.cloneNode(true);
+  // Now we are querying our cloned fragment, not the entire page.
+  const { name, season, number, image, summary } = film;
+  const title = `${name} - S${String(season).padStart(2, "0")}E${String(number
+  ).padStart(2, "0")}`;
+  card.querySelector("h3").textContent = title;
+  card.querySelector("summary").textContent = summary.replace(/<\/?p>/g, "");
+  // console.log(image.medium)
+  card.querySelector("img").setAttribute("src", image.medium);
+  // Return the card, rather than directly appending it to the page
+  return card;
+};
+
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
-  const firstEpisode = allEpisodes[0];
-
-  const template = document.getElementById("film-card");
-  const createFilmCard = (firstEpisode) => {
-    const card = template.content.cloneNode(true);
-
-    // Now we are querying our cloned fragment, not the entire page.
-    const { name, season, number, image, summary } = firstEpisode;
-    const title = `${name} - S${String(season).padStart(2, "0")}E${String(number).padStart(2, "0")}`
-    card.querySelector("h3").textContent = title;
-    card.querySelector("summary").textContent = summary.replace(/<\/?p>/g, "");
-    // console.log(image.medium)
-    card.querySelector("img").setAttribute("src", image.medium);
-    // Return the card, rather than directly appending it to the page
-    return card;
-  };
-  const filmCard = createFilmCard(firstEpisode);
-
+  const filmCard = allEpisodes.map(createFilmCard)
   // Remember we need to append the card to the DOM for it to appear.
-  document.body.append(filmCard);
+  document.body.append(...filmCard);
 }
 
 
